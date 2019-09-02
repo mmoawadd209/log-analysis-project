451ad0ea5a16a2a4dcb9b209ad75b42237084ce5
mohamed moawad sharaf <mohamed.sh1939@gmail.com>
2019-09-02 13:08:22 +0200

    Update log-results.py

--- log-results.py
+++ log-results.py
@@ -4,26 +4,16 @@ import psycopg2
 
 DBNAME = "news"
 
-firstQuery = ("SELECT title, count(*) as views FROM articles \n"
-           "  JOIN log\n"
-           "    ON articles.slug = substring(log.path, 10)\n"
-           "    GROUP BY title ORDER BY views DESC LIMIT 3;")
+articles_query = " SELECT * FROM popular_articles LIMIT 3;"
 
-secondQuery = ("SELECT authors.name, count(*) as views\n"
-           "    FROM articles \n"
-           "    JOIN authors\n"
-           "      ON articles.author = authors.id \n"
-           "      JOIN log \n"
-           "      ON articles.slug = substring(log.path, 10)\n"
-           "      WHERE log.status LIKE '200 OK'\n"
-           "      GROUP BY authors.name ORDER BY views DESC;")
+authors_query  = " SELECT * FROM popular_authors;"
 
-thirdQuery = ("SELECT round((stat*100.0)/visitors, 3) as\n"
-           "        result, to_char(errortime, 'Mon DD, YYYY')\n"
-           "        FROM errorcount ORDER BY result desc limit 1;")
+errors_query   = (""" SELECT to_char(date, 'TMMonth DD"," YYYY'),count
+                      FROM request_errors
+                      WHERE count > 1.0;""")
 
 
-def get_queryResults(sql_query):
+def execute_query(sql_query):
     try:
         db = psycopg2.connect(database=DBNAME)
     except psycopg2.Error as e:
@@ -38,22 +28,22 @@ def get_queryResults(sql_query):
     db.close()
     return results
 
-result1 = get_queryResults(firstQuery)
-result2 = get_queryResults(secondQuery)
-result3 = get_queryResults(thirdQuery)
+popular_articles = execute_query(articles_query)
+popular_authors = execute_query(authors_query)
+request_errors = execute_query(errors_query)
 
 
 
-def print_results(query_list):
-    for i in range(len(query_list)):
-        title = query_list[i][0]
-        r = query_list[i][1]
-        print("\t" + "%s - %d" % (title, r) + " views")
+def print_results(query_result):
+    for i in range(len(query_result)):
+        title = query_result[i][0]
+        count = query_result[i][1]
+        print("\t" + "%s - %d" % (title, count) + " views")
     print("\n")
 
 print("What are the most popular articles of all time?")
-print_results(result1)
+print_results(popular_articles)
 print("Who are the most popular article authors of all time?")
-print_results(result2)
+print_results(popular_authors)
 print("On which days more than 1% of the requests led to error?")
-print("\t" + result3[0][1] + " - " + str(result3[0][0]) + "%")
+print("\t" + date + " - " + str(count) + "% errors\n")
